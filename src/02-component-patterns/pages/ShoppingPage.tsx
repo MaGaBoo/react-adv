bee8f8116848bdee528a75c2d5f36b6e500c3f47
María <91290073+MaGaBoo@users.noreply.github.com>
2023-04-23 12:56:37 +0200

    added product components

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,12 +1,24 @@
 import { ProductCard } from "../components/ProductCard"
 
+const product = {
+id: '1',
+title: 'Coffe Mug - Card',
+img: "/coffee-mug.png"
+}
 
 export const ShoppingPage = () => {
-    return(
-        <div>
-        <h1>Shopping Store</h1>
-        <hr />
-        <ProductCard />
-        </div>
-    )
-}
+
+
+  return (
+    <div>
+      <h1>Shopping Store</h1>
+      <hr />
+      <div style={{ display: "flex", flexWrap: "wrap" }}>
+        <ProductCard product={ product } />
+        <ProductCard product={ product } />
+        <ProductCard product={ product } />
+        <ProductCard product={ product } />
+      </div>
+    </div>
+  );
+};
