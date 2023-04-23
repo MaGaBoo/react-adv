d6ef9e8103389a6b33b032aefb3fa1d295ef49a2
María <91290073+MaGaBoo@users.noreply.github.com>
2023-04-23 14:00:09 +0200

    refactor shopping page

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,24 +1,33 @@
-import { ProductCard } from "../components/ProductCard"
+import { ProductButtons, ProductCard, ProductImage, ProductTitle } from "../components/index";
 
 const product = {
-id: '1',
-title: 'Coffe Mug - Card',
-img: "/coffee-mug.png"
-}
+  id: "1",
+  title: "Coffe Mug - Card",
+  img: "/coffee-mug.png",
+};
 
 export const ShoppingPage = () => {
-
-
   return (
     <div>
       <h1>Shopping Store</h1>
       <hr />
       <div style={{ display: "flex", flexWrap: "wrap" }}>
-        <ProductCard product={ product } />
-        <ProductCard product={ product } />
-        <ProductCard product={ product } />
-        <ProductCard product={ product } />
+
+        {/* Component Compound Pattern */}
+
+{/*         <ProductCard product={product}>
+          <ProductCard.Image />
+          <ProductCard.Title title={'Mug'} />
+          <ProductCard.Buttons />
+        </ProductCard> */}
+
+        <ProductCard product={product}>
+          <ProductImage />
+          <ProductTitle title={'Mug'} />
+          <ProductButtons />
+        </ProductCard>
       </div>
     </div>
   );
 };
+ 
