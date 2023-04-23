8ddd2be23cd033214151d7ed5a0ad57a4c7f0be2
María <91290073+MaGaBoo@users.noreply.github.com>
2023-04-23 12:01:26 +0200

    added useProduct custom hook

--- src/02-component-patterns/components/ProductCard.tsx
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -1,14 +1,28 @@
-import styles from '../styles/styles.module.css'
+import useProducts from "../hooks/useProducts";
+import styles from "../styles/styles.module.css"
 
 export const ProductCard = () => {
 
-    return (
-        <div className={styles.productCard}>
-            <img 
-            className={styles.productImg}
-            src="../assets/coffee-mug.png"
-            alt="Coffee mug" 
-            />
-        </div>
-    )
-}
+
+  const { counter, increaseBy } = useProducts()
+
+  return (
+    <div className={styles.productCard}>
+      <img
+        className={styles.productImg}
+        src="/coffee-mug.png"
+        alt="Coffee mug"
+      />
+      <span className={styles.productDescription}>Coffee Mug</span>
+      <div className={styles.buttonsContainer}>
+        <button className={styles.buttonMinus} onClick={() => increaseBy(-1)}>
+          -
+        </button>
+        <div className={styles.countLabel}>{counter}</div>
+        <button className={styles.buttonAdd} onClick={() => increaseBy(+1)}>
+          +
+        </button>
+      </div>
+    </div>
+  );
+};
